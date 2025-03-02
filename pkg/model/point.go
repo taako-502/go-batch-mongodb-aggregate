55c24a69a3dcc5fbd7e1cf3791625e0a9d5f1539
taako-502 <yamamoto.takao502@gmail.com>
2025-03-02 18:07:19 +0900

    fix

--- pkg/model/point.go
+++ pkg/model/point.go
@@ -1,30 +1,25 @@
 package model
 
 import (
-	"math/rand"
-	"time"
+	"math/rand/v2"
 
 	"go.mongodb.org/mongo-driver/v2/bson"
 )
 
 // Point ユーザーのポイントを格納する
 type Point struct {
-	ID     bson.ObjectID `bson:"_id"`
+	ID     bson.ObjectID `bson:"_id,omitempty"`
 	UserID bson.ObjectID `bson:"userId"`
 	Point  int           `bson:"point"`
 }
 
 func GeneratePoints(userIDs []bson.ObjectID, numberOfPoints int) []Point {
 	var generatedPoints []Point
-	source := rand.NewSource(time.Now().UnixNano())
-	r := rand.New(source)
-
 	for _, ID := range userIDs {
 		for range numberOfPoints {
 			generatedPoints = append(generatedPoints, Point{
-				ID:     bson.NewObjectID(),
 				UserID: ID,
-				Point:  r.Intn(2000) + 1, // 1〜2000のランダムな値
+				Point:  rand.IntN(2000) + 1, // 1〜2000のランダムな値
 			})
 		}
 	}
